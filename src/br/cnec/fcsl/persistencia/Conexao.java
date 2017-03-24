13a0d784f776cde051cd46c66a5d296517de74e7
olive <olive@Danilo-Pc>
2017-03-23 23:41:31 -0300

    conexao com sqlserver

--- src/br/cnec/fcsl/persistencia/Conexao.java
+++ src/br/cnec/fcsl/persistencia/Conexao.java
@@ -4,16 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Conexao {
-	//Classe para conexao ao banco de dados
+	// Classe para conexao ao banco de dados
 	public static Connection getConexao() {
 
 		try {
-			Class.forName(ParametrosMySql.DRIVER);
-			Connection conexao = DriverManager.getConnection(ParametrosMySql.URL, ParametrosMySql.USUARIO,
-					ParametrosMySql.SENHA);
+			Connection conexao = DriverManager.getConnection(
+					"jdbc:sqlserver://server7l.database.windows.net:1433;database=db_lp3;user=danilo@server7l;"
+					+ "password={nti#2017};encrypt=true;trustServerCertificate=false;"
+					+ "hostNameInCertificate=*.database.windows.net;loginTimeout=30");
 			return conexao;
 		} catch (Exception e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 			return null;
 		}
 	}
